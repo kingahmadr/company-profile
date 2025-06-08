9a828f0e9fa909fd7b1be09625b0c1f187b8a9f2
onlyirul <onlyirul@gmail.com>
2025-06-08 14:36:19 +0700

    add href on mobile navigation

--- components/navigationOnPage.tsx
+++ components/navigationOnPage.tsx
@@ -72,7 +72,7 @@ export function NavigationOnPage() {
             {["home", "services", "about", "contact"].map((item) => (
               <button
                 key={item}
-                onClick={() => scrollToSection(item)}
+                onClick={() => router.push("/")}
                 className="text-gray-700 hover:text-blue-600 block px-3 py-2 rounded-md text-base font-medium w-full text-left capitalize"
               >
                 {item}
